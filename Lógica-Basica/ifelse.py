0bbd934e48d6ea65b7e525c25ebf5704c4ee139e
Gustavo Freitas <gustavofreitasdasilva1408@gmail.com>
2025-11-24 20:32:53 -0300

    if e else

---
+++ Lógica-Basica/ifelse.py
@@ -0,0 +1,11 @@
+#if / elif        / else
+# se / se não se  / se não
+
+entrada = input("Voce quer 'entrar' ou 'sair'? ")
+
+if entrada == 'entrar':
+    print("voce entrou")
+elif entrada == 'sair':
+    print("voce saiu")
+else:
+    print("voce nao digitou entrar e nem sair")

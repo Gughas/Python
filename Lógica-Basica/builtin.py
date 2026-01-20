c0c5c2ced6586aa8782ba1656b12af2c4cbdd123
Gustavo Freitas <gustavofreitasdasilva1408@gmail.com>
2026-01-20 10:28:57 -0300

    push

---
+++ Lógica-Basica/builtin.py
@@ -0,0 +1,10 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+Imutável = Aquilo que não pode mudar
+"""
+string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(10))
